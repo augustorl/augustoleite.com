5d66c2c7460c9aecda87b133867c723a6719e3d6
augustorl <gutin22@gmail.com>
2020-11-23 23:17:35 -0300

    adding packages and components to index

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,9 @@
 import Head from "next/head";
 import Header from '../components/Header';
+import Footer from '../components/Footer';
+import Form from '../components/Form';
+
+
 
 export default function Home() {
   return (
@@ -13,6 +17,8 @@ export default function Home() {
       />
       </Head>
       <Header/>
+      <Form/>
+      <Footer/>
     </>
   )
 }
