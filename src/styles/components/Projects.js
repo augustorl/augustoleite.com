acaf0edd42a05a8218878d25b5db0142e67c338b
augustorl <gutin22@gmail.com>
2021-03-14 01:59:41 -0300

    Fix: ProjectContainer css

--- src/styles/components/Projects.js
+++ src/styles/components/Projects.js
@@ -3,21 +3,22 @@ import styled from 'styled-components';
 export const ProjectContainer = styled.section`
    {
     width: 100%;
-    background: #3bd783;
+    background: var(--gradient);
     height: 100%;
     display: flex;
     flex-direction: column;
     justify-content: center;
     align-items: center;
-    padding-bottom: 30px;
+    padding-bottom: 35px;
+    color: rgba(0, 0, 0, 0.8);
     #main {
       background: none !important;
     }
     .swiper-pagination-bullets {
-      margin-top: 20px;
+      bottom: 0px;
     }
     @media (max-width: 866px) {
-      height: 620px;
+      height: 650px;
 
       .swiper-button-next,
       .swiper-button-prev {
@@ -49,11 +50,11 @@ export const TitleContainer = styled.div`
     max-width: 1220px;
     margin: 0 auto;
     padding: 2rem;
+    padding-top: 10px;
     display: flex;
     justify-content: space-between;
     }
   h1 {
-    color: rgba(0, 0, 0, 0.8);
     font-size: 30px;
     letter-spacing: 0.07rem;
     font-family: 'EksellDisplayWeb-Stencil', serif !important;
@@ -68,13 +69,13 @@ export const TitleContainer = styled.div`
     width: 50px;
     height: 4px;
     background-color: rgba(255, 255, 255, 0.8);
-    margin: 0.9rem 0;
+    margin: 0.4rem 0;
   }
 
   @media (max-width: 866px) {
     h1 {
       font-size: 2.6rem;
-      padding-top: 15px;
+      margin-top: 15px;
       text-align: center;
     }
     h1:after {
