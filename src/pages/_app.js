74811f263ceb15bb5a58352afe7e640dbb24dfd7
augustorl <gutin22@gmail.com>
2021-03-14 01:58:30 -0300

    add: Wraper to _app

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,12 +1,12 @@
 import GlobalStyle from '../styles/GlobalStyle';
-
+import Wrapper from '../components/Wrapper';
 
 
 export default function App({ Component, pageProps }) {
   return (
-    <>
+    <Wrapper>
       <GlobalStyle />
         <Component {...pageProps} />
-    </>
+    </Wrapper>
   )
 }
