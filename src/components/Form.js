8c8d24bfa212e7337ca4f034042887ce0916b067
augustorl <gutin22@gmail.com>
2021-03-14 01:55:38 -0300

    Fix: Form Component

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,95 +1,84 @@
-import React, { useState } from 'react';
-import axios from 'axios';
-import { Formik, Form, Field } from 'formik';
-// import { FormattedMessage, useIntl } from 'react-intl';
-
+import React, { useState, useContext, useRef } from 'react';
+// import axios from 'axios';
+import { Context } from '../components/Wrapper';
+import Input from '../components/Input'
+import { Form, setErrors } from '@unform/web'
+import TextArea from '../components/Textarea'
 import * as Yup from 'yup';
-import { Container, ContactForm, ErroredMessage } from '../styles/components/Form';
+import { Container, ContactForm } from '../styles/components/Form';
 
 // Recaptcha HTML 6LcEkMIUAAAAAB8PA3S9CAle8rF58B1EuT1s2HBO
 
-const formSchema = Yup.object().shape({
-  email: Yup.string()
-    .email('Invalid email')
-    .required('Required'),
-  message: Yup.string().required('Required'),
-});
+export default function MyForm() {
+  const formRef = useRef(null);
 
-export default () => {
-//   const intl = useIntl();
-  /* Server State Handling */
-  const [serverState, setServerState] = useState();
-  const handleServerResponse = (ok, msg) => {
-    setServerState({ ok, msg });
-  };
+  async function handleSubmit(data) {
+    try {
+        formRef.current.setErrors({});
 
-  const handleOnSubmit = (values, actions) => {
-    axios({
-      method: 'POST',
-      url: 'https://formspree.io/mzbbvrpq',
-      data: values,
-    }).catch(error => {
-      actions.setSubmitting(false);
-      handleServerResponse(false, error.response.data.error);
-    });
-  };
+        const schema = Yup.object().shape({
+            name: Yup.string().required('(*) Por favor, digite seu nome.'),
+            email: Yup.string().email('Digite um e-mail válido.').required('(*) Por favor, digite seu e-mail preferido.'),
+            message: Yup.string().required('(*) Por favor, preencha a sua mensagem.')
+        })
+        
+    
+        await schema.validate(data, {
+            abortEarly: false,
+        })
+
+
+        console.log(data);
+    } catch(err) {
+        if(err instanceof Yup.ValidationError) {
+            const errorMessages = {};
+
+            err.inner.forEach(error => {
+                errorMessages[error.path] = error.message;
+            })
+
+            formRef.current.setErrors(errorMessages);
+        }
+    }
+}
+
+  const context = useContext(Context);
+
+  const translatedText = context.translation;
 
   return (
     <div>
       <Container>
         <ContactForm>
           <h1>
-            {/* <FormattedMessage id="contact.title" defaultMessage="Contato" /> */}
-            Contato
-          </h1>
-          <Formik
-            initialValues={{ email: '', message: '' }}
-            onSubmit={handleOnSubmit}
-            validationSchema={formSchema}
-          >
-            {({ isSubmitting }) => (
-              <Form id="fs-frm" noValidate>
+            {translatedText.contactTitle}
+           </h1>
+          <Form ref={formRef} onSubmit={handleSubmit}>
                 <ul>
                   <li>
                     <label htmlFor="name">
-                      <Field
+                      <Input
                         name="name"
                         id="name"
-                        placeholder="name"
-                        // placeholder={intl.formatMessage({
-                        //   id: 'name.placeholder',
-                        // })}
+                        placeholder={translatedText.namePlaceholder}
                       />
                     </label>
                     <label htmlFor="email">
-                      <Field
+                      <Input
                         id="email"
                         type="email"
                         name="email"
                         placeholder="E-mail"
                       />
-                      <ErroredMessage
-                        name="email"
-                        className="errorMsg"
-                        component="p"
-                      />
                     </label>
                   </li>
                   <li>
                     <label htmlFor="message">
-                      <Field
+                      <TextArea
                         id="message"
                         name="message"
                         component="textarea"
-                        placeholder="mensagem"
-                        // placeholder={intl.formatMessage({
-                        //   id: 'message.placeholder',
-                        // })}
-                      />
-                      <ErroredMessage
-                        name="message"
-                        className="errorMsg"
-                        component="p"
+                        placeholder={translatedText.messagePlaceholder}
                       />
                     </label>
 
@@ -98,24 +87,12 @@ export default () => {
                       data-sitekey="6LcEkMIUAAAAAB8PA3S9CAle8rF58B1EuT1s2HBO"
                       data-callback="onSubmit"
                       type="submit"
-                      disabled={isSubmitting}
                     >
-                      {/* <FormattedMessage
-                        id="button.content"
-                        defaultMessage="Enviar"
-                      /> */}
-                      Enviar
+                      {translatedText.buttonContent}
                     </button>
                   </li>
                 </ul>
-                {serverState && (
-                  <p className={!serverState.ok ? 'errorMsg' : ''}>
-                    {serverState.msg}
-                  </p>
-                )}
-              </Form>
-            )}
-          </Formik>
+            </Form>
         </ContactForm>
       </Container>
     </div>
