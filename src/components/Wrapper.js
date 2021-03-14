13019770c328796821d8b6cd783870e86be2fecb
augustorl <gutin22@gmail.com>
2021-03-14 01:57:59 -0300

    Add Context

---
+++ src/components/Wrapper.js
@@ -0,0 +1,30 @@
+import React, { useState, createContext } from 'react';
+import { useRouter } from 'next/router';
+import PropTypes from 'prop-types';
+import Portugues from '../languages/pt-BR.json';
+import English from '../languages/en-US.json';
+
+export const Context = createContext();
+
+const Wrapper = props => {
+  const router = useRouter();
+  const { locale } = router;
+
+  const translation = locale === 'pt' ? Portugues : English;
+
+  function selectLanguage(e) {
+    const newLocale = e.target.value;
+
+    router.push("/", "/", { locale: newLocale });
+  };
+  return (
+    <Context.Provider value={{ locale, selectLanguage, translation }}>
+      {props.children}
+    </Context.Provider>
+  );
+};
+
+Wrapper.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+export default Wrapper;
