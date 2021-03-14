ad2a6c0e1cc883f1cc55c4b01e6c6f22791630f0
augustorl <gutin22@gmail.com>
2021-03-14 01:57:41 -0300

    add: Translation to projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,16 +1,19 @@
-import React from 'react';
+import React, { useContext } from 'react';
 // import { FormattedMessage } from 'react-intl';
 import ProjectSlider from './ProjectSlider';
-
+import { Context } from '../components/Wrapper';
 import { ProjectContainer, ProjectInfo, TitleContainer } from '../styles/components/Projects';
 
 export default function Projects() {
+  const context = useContext(Context);
+
+  const translatedText = context.translation;
+
   return (
       <ProjectContainer>
         <TitleContainer>
           <h1>
-            {/* <FormattedMessage id="projects.header" defaultMessage="Projetos" /> */}
-            Projetos
+            {translatedText.projectsHeader}
           </h1>
         </TitleContainer>
         <ProjectInfo>
