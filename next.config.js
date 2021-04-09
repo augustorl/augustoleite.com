49547327bc1fc26629ea06b366ba50b2e573c90a
augustorl <gutin22@gmail.com>
2021-04-08 22:49:01 -0300

    update next config

--- next.config.js
+++ next.config.js
@@ -10,7 +10,7 @@ module.exports = {
         defaultLocale: 'en-US',
       },
       {
-        domain: 'augustoleite.com',
+        domain: 'augustoleite.com/pt',
         defaultLocale: 'pt-BR',
       },
     ],
