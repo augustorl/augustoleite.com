7c064a51112be36c57ecc1557381bd3d5bce4691
augustorl <gutin22@gmail.com>
2021-04-08 22:32:32 -0300

    remove domain routing

--- next.config.js
+++ next.config.js
@@ -2,16 +2,5 @@ module.exports = {
     i18n: {
         locales: ['pt-BR', 'en-US'],
         defaultLocale: 'pt-BR',
-
-        domains: [
-            {
-              domain: 'augustoleite.com',
-              defaultLocale: 'pt-BR',
-            },
-            {
-              domain: 'augustoleite.com/en',
-              defaultLocale: 'en-US',
-            },
-          ],
     }
 }
